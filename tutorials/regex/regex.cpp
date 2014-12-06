c993dfe5db0a5d235d76b199f2650ab0378a6903
Thomas Liu <liniarc@gmail.com>
2014-12-06 14:14:30 -0800

    added mouse support and support for extended regex

--- tutorials/regex/regex.cpp
+++ tutorials/regex/regex.cpp
@@ -13,14 +13,50 @@ int x = 0, y = 0;
 string regexPattern;
 string replacePattern;
 regex r;
-bool valid = false;
-string words[10] = {"apple", "apex", "air", "annoy", "aardvark", "apply", "bot", "brains", "crayon", "master"};
+
 vector<string> lines;
+
+bool extended = false;
+bool global = false;
+bool replaceMode = false;
+bool finished = false;
+
+void mouseAction(int mx, int my)
+{
+    if (my == 2)
+    {
+        if (mx < 18)
+            extended = !extended;
+        else if (mx > 22 && mx < 36)
+            global = !global;
+        else if (mx > 38 && mx < 57)
+            replaceMode = !replaceMode;
+    }
+    else if (my == 0)
+    {
+        y = 0;
+        x = mx-16;
+        if (mx-16 > regexPattern.length())
+            x = regexPattern.length();
+    }
+    else if (my == 1)
+    {
+        y = 1;
+        x = mx-16;
+        if (mx-16 > replacePattern.length())
+            x = replacePattern.length();
+    }
+}
 
 void keyAction( int ch )
 {
     switch (ch)
     {
+        case KEY_MOUSE:
+            MEVENT event;
+            getmouse(&event);
+            mouseAction(event.x,event.y);
+            break;
         case KEY_BACKSPACE:
             if (y == 0 && x > 0 && regexPattern.length() >= x)
             {
@@ -68,30 +104,52 @@ void keyAction( int ch )
 void drawScreen()
 {
     clear();
+    move(0,0);
+    addstr("Regex Pattern: ");
+    move(0,16);
     addstr(regexPattern.c_str());
-    addstr("\n");
+    move(1,0);
+    addstr("Replace With:  ");
+    move(1,16);
     addstr(replacePattern.c_str());
-    addstr("\n");
-    addstr("\n");
+    move(2,0);
+    addstr("Extended Regex [ ]    Global Tag [ ]    Replace Mode [ ]");
+    
+    if (extended)
+        mvaddch(2,16,'X');
+    if (global)
+        mvaddch(2,34,'X');
+    if (replaceMode)
+        mvaddch(2,54,'X');
+    
+    move(3,0);
+    bool valid = true;
     try
-    {
-        r.assign(regexPattern.c_str(), regex::basic);
-        valid = true;
+    {  
+        if (extended)
+        {
+            r.assign(regexPattern.c_str(), regex::extended);
+        }
+        else
+        {
+            if (regexPattern.back() != '\\')
+                r.assign(regexPattern.c_str(), regex::basic);
+            else
+                valid = false;
+        }
     }
     catch (regex_error &e)
     {
         addstr("Invalid regex!");
         valid = false;
     }
-    addstr("\n");
-    addstr("\n");
     for (int i = 0; i < lines.size(); i++)
     {
         int color = 1;
+        move(5+i,0);
         if (!valid)
         {
             addstr(lines[i].c_str());
-            addstr("\n");
             continue;
         }
         smatch m;
@@ -101,7 +159,6 @@ void drawScreen()
 
         init_pair(1, COLOR_BLACK, COLOR_GREEN);
         init_pair(2, COLOR_BLACK, COLOR_BLUE);
-
         for (sregex_iterator j = words_begin; j != words_end; j++)
         {
             m = *j;
@@ -117,13 +174,13 @@ void drawScreen()
             addstr(s.c_str());
         else
             addstr(m.suffix().str().c_str());
-        addstr("     ");
-        if (replacePattern.length() != 0)
+        move(5+i, 50); 
+        if (replaceMode)
+        {
             addstr(regex_replace(s,r,replacePattern).c_str());
-        addstr("\n");
-        
+        }
     }
-    move(y, x);
+    move(y, x+16);
 }
 
 void getFile(string s)
@@ -144,6 +201,7 @@ int main ()
     use_default_colors();
     cbreak();
     noecho();
+    mousemask(BUTTON1_PRESSED, NULL);
     keypad(stdscr, TRUE);
     drawScreen();
     while(ch!= 'q')
